bc87dde966a4d5288b0d38d7c549af590d304945
Devin Elliot <devinjelliot@gmail.com>
2024-05-21 23:06:44 -0700

    adds payment widget to checkout view in iframe

--- app/public/wp-content/plugins/custom-payment-gateway/custom-payment-gateway.php
+++ app/public/wp-content/plugins/custom-payment-gateway/custom-payment-gateway.php
@@ -4,9 +4,9 @@
  * Plugin Name: WooCommerce Custom Payment Gateway
  * Plugin URI:
  * Description: WooCommerce Custom Payment Gateway
- * Author: BP - Devin
+ * Author: DEV Team
  * Author URI:
- * Version: 1.1.0
+ * Version: 1.0.5
  */
 
 add_filter('woocommerce_payment_gateways', 'client33_add_gateway_class');
@@ -28,6 +28,8 @@ function client33_init_gateway_class() {
             $this->method_description = '';
             $this->supports = array('products');
 
+            $this->order_total = WC()->cart->total;
+
             $this->init_form_fields();
             $this->init_settings();
 
@@ -35,7 +37,6 @@ function client33_init_gateway_class() {
             $this->description = $this->get_option('description');
             $this->enabled = $this->get_option('enabled');
 
-            add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
             add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
         }
 
@@ -65,101 +66,45 @@ function client33_init_gateway_class() {
         }
 
         public function payment_fields() {
-            ?>
-            <fieldset>
-                <p class="form-row form-row-wide">
-                    <label for="cc_number"><?php _e('Card number', 'woocommerce'); ?> <span class="required">*</span></label>
-                    <input id="cc_number" name="cc_number" type="text" autocomplete="off" placeholder="1234 1234 1234 1234">
-                    <img src="https://example.com/path-to-your-icons/visa.png" alt="Visa">
-                    <img src="https://example.com/path-to-your-icons/mastercard.png" alt="MasterCard">
-                    <img src="https://example.com/path-to-your-icons/amex.png" alt="American Express">
-                    <img src="https://example.com/path-to-your-icons/elo.png" alt="Elo">
-                </p>
-                <p class="form-row form-row-first">
-                    <label for="cc_expiry"><?php _e('Expiration', 'woocommerce'); ?> <span class="required">*</span></label>
-                    <input id="cc_expiry" name="cc_expiry" type="text" autocomplete="off" placeholder="MM / YY">
-                </p>
-                <p class="form-row form-row-last">
-                    <label for="cc_cvc"><?php _e('CVC', 'woocommerce'); ?> <span class="required">*</span></label>
-                    <input id="cc_cvc" name="cc_cvc" type="text" autocomplete="off" placeholder="CVC">
-                    <img src="https://example.com/path-to-your-icons/cvc.png" alt="CVC">
-                </p>
-                <div class="clear"></div>
-            </fieldset>
-            <?php
+            if (isset($_POST['coupon_code'])) {
+                $coupon_code = sanitize_text_field($_POST['coupon_code']);
+
+
+                $result = WC()->cart->apply_coupon($coupon_code);
+
+                if ($result === true) {
+                    // Coupon applied successfully, recalculate the order total
+                    $orderTotal = $order->calculate_totals();
+                } else {
+                    // Handle coupon application error
+                    wc_add_notice($result, 'error');
+                }
+            } else {
+                $orderTotal = $this->order_total;
+            }
+
+            echo do_shortcode('[payment-form-shortcode order_total="' . $orderTotal . '"]');
         }
 
         public function payment_scripts() {
-            if (!is_checkout() || !$this->enabled) {
-                return;
-            }
+            // Intentionally left blank
         }
 
         public function validate_fields() {
-            if (empty($_POST['cc_number']) || empty($_POST['cc_expiry']) || empty($_POST['cc_cvc'])) {
-                wc_add_notice(__('Please fill in all required fields.', 'woocommerce'), 'error');
-                return false;
-            }
-
-            // Validate credit card number using Luhn algorithm
-            if (!$this->is_valid_card_number($_POST['cc_number'])) {
-                wc_add_notice(__('Invalid card number.', 'woocommerce'), 'error');
-                return false;
-            }
-
-            // Validate expiry date format
-            if (!preg_match('/^(0[1-9]|1[0-2]) \/ ([0-9]{2})$/', $_POST['cc_expiry'])) {
-                wc_add_notice(__('Invalid expiry date format. Use MM / YY.', 'woocommerce'), 'error');
-                return false;
-            }
-
-            // Validate CVC format
-            if (!preg_match('/^[0-9]{3,4}$/', $_POST['cc_cvc'])) {
-                wc_add_notice(__('Invalid CVC code.', 'woocommerce'), 'error');
-                return false;
-            }
-
             return true;
         }
 
-        private function is_valid_card_number($number) {
-            $number = preg_replace('/\D/', '', $number);
-            $checksum = 0;
-            $length = strlen($number);
-
-            for ($i = (2 - ($length % 2)); $i <= $length; $i += 2) {
-                $checksum += (int)($number[$i - 1]);
-            }
-
-            for ($i = ($length % 2) + 1; $i < $length; $i += 2) {
-                $digit = (int)($number[$i - 1]) * 2;
-                if ($digit < 10) {
-                    $checksum += $digit;
-                } else {
-                    $checksum += ($digit - 9);
-                }
-            }
-
-            return ($checksum % 10) === 0;
-        }
-
         public function process_payment($order_id) {
-            global $woocommerce;
-            $order = new WC_Order($order_id);
+            $order = wc_get_order($order_id);
 
-            // Set to true to test payment error
-            $payment_error = false;
+            // Mark the order as processing or completed
+            $order->update_status('processing', __('Payment received, your order is now being processed.', 'woocommerce'));
 
-            if ($payment_error) {
-                wc_add_notice(__('Payment error: ' . $payment_error, 'woocommerce'), 'error');
-                return false;
-            }
-
-            // Payment is successful
-            $order->payment_complete();
+            // Reduce stock levels
+            wc_reduce_stock_levels($order_id);
 
             // Remove cart
-            $woocommerce->cart->empty_cart();
+            WC()->cart->empty_cart();
 
             // Return thank you page redirect
             return array(
@@ -168,8 +113,7 @@ function client33_init_gateway_class() {
             );
         }
 
-        public function webhook()
-        {
+        public function webhook() {
             // Intentionally left blank
         }
     }
--- app/public/wp-content/plugins/my-custom-features/my-custom-features.php
+++ app/public/wp-content/plugins/my-custom-features/my-custom-features.php
@@ -4,134 +4,210 @@
  * Plugin Name: My Custom Features
  * Plugin URI:
  * Description: Various custom features developed by Dev Team
- * Version: 1.0.11
- * Author: BP - Devin
+ * Version: 1.0.5
+ * Author: DEV Team - Thomas
  * Author URI:
  */
 
-// Register activation hook
-register_activation_hook(__FILE__, 'mcf_create_required_pages');
-// Add admin menu
 add_action('admin_menu', 'create_custom_feature_page');
-// Add shortcodes
+add_shortcode('payment-form-shortcode', 'payment_form');
 add_shortcode('thank-you-shortcode', 'thank_you');
-// Add admin menu page
+register_activation_hook(__FILE__, 'mcf_create_required_pages');
+// Hook to add custom payment form to WooCommerce checkout page
+//add_action('woocommerce_review_order_before_payment', 'add_custom_payment_form_to_checkout');
+
 function create_custom_feature_page() {
 
-    $page_title = 'Custom Payment Plugin';
-    $menu_title = 'Custom Payment Plugin';
-    $capability = 'read';
-    $slug = 'custom_feature_page';
-    $callback = 'custom_feature_content';
-    $icon = 'dashicons-welcome-write-blog';
-    $position = 100;
-    add_menu_page($page_title, $menu_title, $capability, $slug, $callback, $icon, $position);
+    add_menu_page('Custom Features', 'Custom Features', 'manage_options', 'custom_feature_page', 'custom_feature_content');
 }
 
-// Admin page content
 function custom_feature_content() {
 
     ?>
-    <style type="text/css">
+  <style type="text/css">
         .btn_update_settings {
-            cursor: pointer;
-            padding: 10px 15px;
+         cursor: pointer;
+           padding: 10px 15px;
         }
-        .textbox-large {
-            width: 550px !important;
-        }
-        .textbox-medium {
-            width: 350px !important;
-        }
-        .textbox-small {
-            width: 200px !important;
-        }
-        .margin-top-10 {
-            margin-top: 10px;
-        }
-        .successMsg {
-            color: #006600;
-            font-weight: bold;
-        }
-    </style>
 
-    <h1 class="margin-top-10">Settings</h1>
+     .textbox-large {
+           width: 550px !important;
+       }
+
+     .textbox-medium {
+          width: 350px !important;
+       }
+
+     .textbox-small {
+           width: 200px !important;
+       }
+
+     .margin-top-10 {
+           margin-top: 10px;
+      }
+  </style>
+
+  <h1 class="margin-top-10">Settings</h1>
     <?php
-    $GLOBALS['msg'] = '';
+    $msg = '';
     if (isset($_POST['btn_update_settings']) && $_REQUEST['mode'] == 'update_custom_feature_settings') {
         update_option('client_payment_page_url', $_POST['client_payment_page_url']);
-        $GLOBALS['msg'] = 'Settings updated successfully.';
+        $msg = 'Settings updated successfully.';
     }
     ?>
 
-    <form name="frmUpdateCustomFeatureSettings" method="POST" action="admin.php?page=custom_feature_page&mode=update_custom_feature_settings">
-        <table cellpadding="10" cellspacing="10">
-            <?php if (!empty($GLOBALS['msg'])) {
+  <form name="frmUpdateCustomFeatureSettings" method="POST"
+      action="admin.php?page=custom_feature_page&mode=update_custom_feature_settings">
+       <table cellpadding="10" cellspacing="10">
+            <?php if (!empty($msg)) {
                 ?>
-                <tr>
-                    <td colspan="2" class="successMsg"><?php echo stripslashes($GLOBALS['msg']); ?></td>
-                </tr>
+             <tr>
+                    <td colspan="2" class="successMsg"><?php echo stripslashes($msg); ?></td>
+             </tr>
                 <?php
             } ?>
-            <tr>
-                <td><strong>Client URL</strong></td>
-                <td>
-                    <input type="text" name="client_payment_page_url" class="textbox-medium" value="<?php echo get_option('client_payment_page_url'); ?>" placeholder="Enter Client URL" required/>
+
+          <tr>
+               <td><strong>Client URL</strong></td>
+               <td>
+                   <input type="text" name="client_payment_page_url" class="textbox-medium"
+                        value="<?php echo get_option('client_payment_page_url'); ?>" placeholder="Enter Client URL"
+                       required />
                 </td>
-            </tr>
-            <tr>
-                <td>&nbsp;</td>
-                <td>
-                    <input name="btn_update_settings" class="btn_update_settings" type="submit" value="Submit" />
-                </td>
-            </tr>
-        </table>
-    </form>
+          </tr>
+
+         <tr>
+               <td>&nbsp;</td>
+                <td><input name="btn_update_settings" class="btn_update_settings" type="submit" value="Submit" /></td>
+         </tr>
+      </table>
+   </form>
     <?php
 }
 
-// Create required pages on plugin activation
+function payment_form() {
+    ob_start();
+    if (!empty($_REQUEST['order_ref'])) {
+        $order_id = base64_decode(sanitize_text_field($_REQUEST['order_ref']));
+        $order = wc_get_order($order_id);
+        if ($order) {
+            $order_total = $order->get_total();
+            $order_ref = site_url() . '||' . $order_id;
+            $order_ref = base64_encode($order_ref);
+            $url = get_option('client_payment_page_url');
+
+            if (empty($url)) {
+                die("Error: URL is empty.");
+            }
+
+            if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+                die("Error: Invalid URL provided.");
+            }
+
+            $crl = curl_init();
+            if ($crl === false) {
+                die("Error: Unable to initialize cURL session.");
+            }
+
+            curl_setopt($crl, CURLOPT_URL, $url);
+            curl_setopt($crl, CURLOPT_FRESH_CONNECT, true);
+            curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($crl);
+            if ($response === false) {
+                die("Error: \"" . curl_error($crl) . "\" - Code: " . curl_errno($crl));
+            }
+
+            curl_close($crl);
+            $response = str_replace('"', '', $response);
+            ?>
+            <style>
+                .iframe-payment {
+                    width: 600px;
+                    height: 480px;
+                }
+                @media(max-width: 282px) {
+                    .iframe-payment {
+                        width: 100%;
+                        height: 680px !important;
+                    }
+                }
+                @media(max-width: 480px) {
+                    .iframe-payment {
+                        width: 100%;
+                        height: 480px;
+                    }
+                }
+            </style>
+            <iframe src="<?php echo esc_url($response); ?>?order_total=<?php echo esc_attr($order_total); ?>&order_ref=<?php echo esc_attr($order_ref); ?>" class="iframe-payment" style="border:none;"></iframe>
+            <?php
+        } else {
+            echo '<p align="center" style="color:#FF0000;">Invalid Order!</p>';
+        }
+    } else {
+        echo '<p align="center" style="color:#FF0000;">Invalid Order!</p>';
+    }
+    return ob_get_clean();
+}
+
+function thank_you() {
+
+    ob_start();
+    if (!empty($_REQUEST['order_ref'])) {
+        $order_id = base64_decode(sanitize_text_field($_REQUEST['order_ref']));
+        $order = wc_get_order($order_id);
+        if (!$order) {
+            echo '<p><strong>Invalid Order!</strong></p>';
+            return ob_get_clean();
+        }
+        $redirect_url = $order->get_checkout_order_received_url();
+        if ($order->get_status() == 'processing') {
+            ?>
+            <form action="<?php echo esc_url($redirect_url); ?>" name="frmRedirectSuccessPage" method="get">
+                <input type="hidden" name="key" value="<?php echo esc_attr($order->get_order_key()); ?>">
+           </form>
+            <script language="javascript">document.frmRedirectSuccessPage.submit();</script>
+            <?php
+            die;
+        } else {
+            ?>
+            <p><strong>Invalid Order!</strong></p>
+            <?php
+        }
+    }
+    return ob_get_clean();
+}
+
 function mcf_create_required_pages() {
 
-    $pages = [
-        'thank-you' => [
-            'title' => 'Thank You',
-            'content' => '[thank-you-shortcode]',
-        ],
-    ];
-    foreach ($pages as $slug => $page) {
-        if (!get_page_by_path($slug, OBJECT, 'page')) {
-            wp_insert_post([
-                'post_type' => 'page',
-                'post_title' => $page['title'],
-                'post_content' => $page['content'],
-                'post_status' => 'publish',
-                'post_author' => 1,
-                'post_name' => $slug,
-                ]);
-        }
+    $page_slug = 'payment';
+    $new_page = array(
+        'post_type' => 'page',
+        'post_title' => 'Payment',
+        'post_content' => '[payment-form-shortcode]',
+        'post_status' => 'publish',
+        'post_author' => 1,
+        'post_name' => $page_slug
+    );
+    if (!get_page_by_path($page_slug, OBJECT, 'page')) {
+        wp_insert_post($new_page);
+    }
+
+    $page_slug = 'thank-you';
+    $new_page = array(
+        'post_type' => 'page',
+        'post_title' => 'Thank You',
+        'post_content' => '[thank-you-shortcode]',
+        'post_status' => 'publish',
+        'post_author' => 1,
+        'post_name' => $page_slug
+    );
+    if (!get_page_by_path($page_slug, OBJECT, 'page')) {
+        wp_insert_post($new_page);
     }
 }
 
-// Shortcode for thank you page
-function thank_you()
-{
-
-    if (!empty($_REQUEST['order_ref'])) {
-        $order_ref = base64_decode($_REQUEST['order_ref']);
-        $order_id = $order_ref;
-        if ($order_id > 0) {
-            if ($order_id > 0) {
-                $order = wc_get_order($order_id);
-                if ($order->has_status('completed')) {
-                    $order_note1 = 'Payment Intent: ' . sanitize_text_field($_REQUEST['payment_intent']);
-                    $order->add_order_note($order_note1);
-                    return '<p>Thank you for your payment. Your order has been completed.</p>';
-                } else {
-                    return '<p>Payment not completed. Please try again.</p>';
-                }
-            }
-        }
-    }
-        return '<p>Invalid order reference.</p>';
-}
+// function add_custom_payment_form_to_checkout()
+// {
+//    echo do_shortcode('[payment-form-shortcode]');
+// }
+// ?>
